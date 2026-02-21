e3d568ef36b41e14c9f6fa2e31f8cb3d58fd6450
Кирилл Романович <k458pro@yandex.ru>
2026-02-21 10:40:16 +0300

    Последние штрихи к 4 лабе

--- ОПД/Lab_4/Тестирование кода из БЭВМ.py
+++ ОПД/Lab_4/Тестирование кода из БЭВМ.py
@@ -3,8 +3,9 @@ def f(x):
         return 4087
     return 4*x - 116
 
-z = int("0FAB", 16)
-y = int("1000", 16)
-x = int("0345", 16)
+z = int("1388", 16)
+y = int("0", 16)
+x = int("1388", 16)
 
+print(hex(5000))
 print(hex(-f(z) + 1 + f(x+1) + 1 + f(y+1))[2:])
