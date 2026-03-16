05a6f5941caf6b90b15b582d6e262567d42b4342
Кирилл Романович <k458pro@yandex.ru>
2026-03-16 17:35:09 +0300

    Первые файлы к 6 лабе по опд

---
+++ ОПД/Lab_6/prog.asm
@@ -0,0 +1,92 @@
+ORG 0x0
+V0: WORD $default, 0x180 
+V1: WORD $default, 0x180
+V2: WORD $int2, 0x180
+V3: WORD $int3, 0x180
+V4: WORD $default, 0x180
+V5: WORD $default, 0x180
+V6: WORD $default, 0x180
+V7: WORD $default, 0x180
+
+ORG 0x042
+X: WORD ?
+
+min: WORD
+max: WORD
+
+temp_int2: WORD ?
+
+default: IRET
+
+START:
+	DI
+	CLA
+
+	; Запрещаем возможность прерывания с неиспользуемых ВУ
+	
+	OUT 0x1
+	OUT 0x3
+	OUT 0xB
+	OUT 0xE
+	OUT 0x12
+	OUT 0x16
+	OUT 0x1A
+	OUT 0x1E
+
+	;Разрешаем возможность прерывания с ВУ, назначая на соответствующие векторы в начале памяти
+	
+	LD #0xA ; (1000|0010 = 1010)
+	OUT 5
+	LD #0xB ; (1000 | 0011 = 1011)
+	EI
+
+
+; Бесконечный цикл с уменьшением X на 3
+
+body:
+	DI
+	LD X
+	SUB 0x3
+	CALL check
+	ST X
+	EI
+	JUMP body
+	
+; Подпрограмма прерывания для ВУ-2
+
+int2:
+	DI
+	ST temp_int2
+	NOP
+	IN 4
+	OR temp_int2
+	ST X
+	NOP
+	EI
+	IRET
+
+; Подпрограмма прерывания для ВУ-3
+	
+int3:
+	DI
+	NOP
+	LD X
+	ASL
+	ADD X
+	SUB #0x3
+	OUT 6
+	NOP
+	EI
+	IRET
+
+; Подпрограмма для проверки X на ОДЗ
+
+check:
+check_min:
+	CMP min
+	BPL check_max 
+check_max:
+	CMP max
+	BMI return
+load_max: LD max
+return: RET
